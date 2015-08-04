f4167564835c423a7d43a4eee4f93e81075450ef
dan <danbickford007@yahoo.com>
2015-08-04 11:06:33 -0400

    build lib files

--- dist/minimongo.js
+++ dist/minimongo.js
@@ -1480,7 +1480,7 @@ Collection = (function() {
 
   Collection.prototype._findFetch = function(selector, options, success, error) {
     if (success != null) {
-      return success(processFind(this.items, selector, options));
+      return processFind(this.items, selector, options);
     }
   };
 
